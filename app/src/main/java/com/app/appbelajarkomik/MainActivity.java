6ffded33737d4e8099e9d2a66244d92176cee085
Firman Taufik <fkopitezz@gmail.com>
2022-08-01 00:22:34 +0700

    COMMIT 1

--- app/src/main/java/com/app/appbelajarkomik/MainActivity.java
+++ app/src/main/java/com/app/appbelajarkomik/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ///TESTTTT KIRIM
     }
 
     public void detail(View view) {
