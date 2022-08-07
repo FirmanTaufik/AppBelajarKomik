7d3a10906629f95b694c810fa3516bcbcffc8d6e
Firman Taufik <fkopitezz@gmail.com>
2022-08-07 23:54:42 +0700

    COMMIT 4

--- app/src/main/java/com/app/appbelajarkomik/DetailActivity.java
+++ app/src/main/java/com/app/appbelajarkomik/DetailActivity.java
@@ -2,19 +2,25 @@ package com.app.appbelajarkomik;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.palette.graphics.Palette;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
-    private FrameLayout frameLeft;
+    private  FrameLayout frameLeft;
     private FrameLayout frameRight;
     private CustomBackgroundCurve customBackgroundCurve;
+    private CardView cardChapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,19 +30,27 @@ public class DetailActivity extends AppCompatActivity {
         customBackgroundCurve = findViewById(R.id.custom);
         frameLeft = findViewById(R.id.frameLeft);
         frameRight = findViewById(R.id.frameRight);
+        cardChapter = findViewById(R.id.cardChapter);
 
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cover);
-        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-            @Override
-            public void onGenerated(@Nullable Palette palette) {
-                int defaultValue = 0x000000;
-                int color = palette.getDarkVibrantColor(defaultValue);
-                customBackgroundCurve.setBg(color);
-                frameLeft.setBackgroundColor(color);
-                frameRight.setBackgroundColor(color);
-              //  customBackgroundCurve.setBg(getResources().getColor(R.color.testColor));
+        Palette.from(bitmap).generate(palette -> {
+            int defaultValue = 0x000000;
+            int color = palette.getDarkVibrantColor(defaultValue);
+            customBackgroundCurve.setBg(color);
+            frameLeft.setBackgroundColor(color);
+            frameRight.setBackgroundColor(color);
+          //  customBackgroundCurve.setBg(getResources().getColor(R.color.testColor));
 
+        });
+
+
+        cardChapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DetailActivity.this, "Show Chapter", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+
 }
