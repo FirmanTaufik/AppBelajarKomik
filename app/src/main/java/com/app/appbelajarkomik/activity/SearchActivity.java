d2edbb0880b8d26bc4328659a776c3bc5b5532c1
firman <fkopitezz@gmail.com>
2022-09-18 00:11:30 +0700

    COMMIT 11

--- app/src/main/java/com/app/appbelajarkomik/activity/SearchActivity.java
+++ app/src/main/java/com/app/appbelajarkomik/activity/SearchActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -12,6 +13,11 @@ import com.app.appbelajarkomik.R;
 import com.app.appbelajarkomik.adapter.rvAdapterSearch;
 import com.app.appbelajarkomik.model.ListSearchModel;
 import com.app.appbelajarkomik.utils.ParsePageTask;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 
@@ -35,13 +41,34 @@ public class SearchActivity extends AppCompatActivity implements ParsePageTask.C
         gridLayoutManager = new GridLayoutManager(this, 3);
         recyclerView.setLayoutManager(gridLayoutManager);
 
+        String keyword = getIntent().getStringExtra("keyword").replaceAll(" ","+");
         ParsePageTask parsePageTask = new ParsePageTask();
-        parsePageTask.execute("https://komikindo.id/");
+        parsePageTask.execute("https://komikindo.id/?s="+keyword );
         parsePageTask.setCallback(this);
     }
 
     @Override
     public void onChange(String response) {
+        progressBar.setVisibility(View.GONE);
+        try {
+            Document document = Jsoup.parse(response);
+            Elements elements = document.getElementsByClass("postBody");
+            Elements list = elements.select("div.film-list").select("div.animepost");
+
+            for (Element l : list){
+                String gambar = l.select("a").select("div.limit").select("img").attr("src");
+                String judul = l.select("div.bigors").select("a").select("div.tt").text();
+                String rating = l.select("div.bigors").select("div.adds").select("div.rating").select("i").text();
+                String link = l.select("a").attr("href");
+
+                listSearchModels.add(new ListSearchModel(judul, gambar, rating,link));
+            }
+
+            adapterSearch.notifyDataSetChanged();
+
+        }catch (NullPointerException n) {
+            Toast.makeText(this, "tidak ada komik yang di cari", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
